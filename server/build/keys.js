cc16b9ef8f8aa46c3ff195ec3bc3c5f93d3da38d
Christian Montes <christian.montes@usantoto.edu.co>
2023-11-29 19:26:15 -0500

    server

---
+++ server/build/keys.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    database: {
+        host: 'localhost',
+        user: 'camilo_r',
+        password: 'juan123',
+        database: 'ng_games',
+        port: 5432
+    }
+};
